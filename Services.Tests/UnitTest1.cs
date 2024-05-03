af452ffc39672e8c4c5b4b33240c79c53ffddefe
aaronmontgomery <aaronmontgomery1989@outlook.com>
2024-05-02 23:39:28 -0400

    added unit test project and Moq dependency

---
+++ Services.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Services.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //Moq
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
